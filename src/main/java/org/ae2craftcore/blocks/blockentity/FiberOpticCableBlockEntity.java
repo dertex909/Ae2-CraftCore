90fd1912f02102b652b82bce4e98159114d060d1
dertex909 <codshannw@gmail.com>
2026-06-13 18:31:35 +0500

    да

--- src/main/java/org/ae2craftcore/blocks/blockentity/FiberOpticCableBlockEntity.java
+++ src/main/java/org/ae2craftcore/blocks/blockentity/FiberOpticCableBlockEntity.java
@@ -1,13 +1,13 @@
 package org.ae2craftcore.blocks.blockentity;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.Connection;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
@@ -17,6 +17,8 @@ import org.ae2craftcore.blocks.block.FiberOpticCableBlock;
 import org.ae2craftcore.blocks.block.SfpModuleBlock;
 import org.ae2craftcore.registry.annotations.RegisterBlockEntity;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
 
 @RegisterBlockEntity(name = "fiber_optic_cable", blocks = {FiberOpticCableBlock.class})
 public class FiberOpticCableBlockEntity extends BlockEntity {
@@ -38,34 +40,47 @@ public class FiberOpticCableBlockEntity extends BlockEntity {
         return connectionMask;
     }
 
-    public void setConnectionMask(int mask) {
-        this.connectionMask = mask;
-    }
-
     public void recalculateConnections() {
         if (this.level == null) return;
+
+        var potentialDirs = new ArrayList<Direction>();
+        for (var dir : Direction.values()) if (canConnectToNeighbor(dir)) potentialDirs.add(dir);
+
+        var finalDirs = new ArrayList<Direction>();
+        for (var dir : potentialDirs) {
+            int dirBit = getDirectionBit(dir);
+            if ((this.connectionMask & dirBit) != 0) finalDirs.add(dir);
+        }
+
+        for (var dir : potentialDirs) {
+            if (finalDirs.size() >= 2) break;
+            if (!finalDirs.contains(dir)) {
+                if (finalDirs.size() == 1) if (dir == finalDirs.getFirst().getOpposite()) finalDirs.add(dir);
+            }
+        }
+
+        for (var dir : potentialDirs) {
+            if (finalDirs.size() >= 2) break;
+            if (!finalDirs.contains(dir)) finalDirs.add(dir);
+        }
+
         int newMask = 0;
-        if (canConnect(this.level, this.worldPosition.north())) newMask |= 1;
-        if (canConnect(this.level, this.worldPosition.east())) newMask |= 2;
-        if (canConnect(this.level, this.worldPosition.south())) newMask |= 4;
-        if (canConnect(this.level, this.worldPosition.west())) newMask |= 8;
-        if (canConnect(this.level, this.worldPosition.above())) newMask |= 16;
-        if (canConnect(this.level, this.worldPosition.below())) newMask |= 32;
+        for (var dir : finalDirs) newMask |= getDirectionBit(dir);
 
         if (this.connectionMask != newMask) {
             this.connectionMask = newMask;
             this.setChanged();
+
             if (this.level.isClientSide) {
                 this.requestModelDataUpdate();
             } else {
                 this.level.sendBlockUpdated(this.worldPosition, this.getBlockState(), this.getBlockState(), 3);
+                for (var dir : Direction.values()) {
+                    var neighborBe = this.level.getBlockEntity(this.worldPosition.relative(dir));
+                    if (neighborBe instanceof FiberOpticCableBlockEntity nCable) nCable.recalculateConnections();
+                }
             }
         }
-    }
-
-    private boolean canConnect(BlockGetter level, BlockPos neighborPos) {
-        var state = level.getBlockState(neighborPos);
-        return state.getBlock() instanceof FiberOpticCableBlock || state.getBlock() instanceof SfpModuleBlock;
     }
 
     @Override
@@ -101,5 +116,48 @@ public class FiberOpticCableBlockEntity extends BlockEntity {
             this.requestModelDataUpdate();
             this.level.sendBlockUpdated(this.worldPosition, this.getBlockState(), this.getBlockState(), 3);
         }
+    }
+
+    private int getDirectionBit(Direction dir) {
+        return switch (dir) {
+            case NORTH -> 1;
+            case EAST -> 2;
+            case SOUTH -> 4;
+            case WEST -> 8;
+            case UP -> 16;
+            case DOWN -> 32;
+        };
+    }
+
+    private int getOppositeBit(Direction dir) {
+        return switch (dir) {
+            case NORTH -> 4;
+            case EAST -> 8;
+            case SOUTH -> 1;
+            case WEST -> 2;
+            case UP -> 32;
+            case DOWN -> 16;
+        };
+    }
+
+    private boolean canConnectToNeighbor(Direction dir) {
+        var neighborPos = this.worldPosition.relative(dir);
+        if (this.level == null) return false;
+
+        var state = this.level.getBlockState(neighborPos);
+        if (!(state.getBlock() instanceof FiberOpticCableBlock || state.getBlock() instanceof SfpModuleBlock))
+            return false;
+        if (state.getBlock() instanceof SfpModuleBlock) return true;
+
+        var be = this.level.getBlockEntity(neighborPos);
+        if (be instanceof FiberOpticCableBlockEntity neighborCable) {
+            int neighborMask = neighborCable.getConnectionMask();
+            int neighborConnectionsCount = Integer.bitCount(neighborMask);
+            if (neighborConnectionsCount < 2) return true;
+            int oppositeBit = getOppositeBit(dir);
+            return (neighborMask & oppositeBit) != 0;
+        }
+
+        return false;
     }
 }
