2c7f296f6687c6482c01abf406ea1fd8b5e96950
dertex909 <codshannw@gmail.com>
2026-06-14 13:25:43 +0500

    да

--- src/main/java/org/ae2craftcore/items/BaseResources.java
+++ src/main/java/org/ae2craftcore/items/BaseResources.java
@@ -17,6 +17,11 @@ import org.ae2craftcore.registry.annotations.RegisterItem;
 @RegisterItem(name = "quantum_scrap")
 
 @RegisterItem(name = "quantum_processor_press", stacksTo = 1)
+
+@RegisterItem(name = "luck_card_1", stacksTo = 16)
+@RegisterItem(name = "luck_card_2", stacksTo = 16)
+@RegisterItem(name = "luck_card_3", stacksTo = 16)
+@RegisterItem(name = "luck_card_4", stacksTo = 16)
 public class BaseResources extends Item {
 
     public static DeferredHolder<Item, BaseResources> OBSIDIAN_PLATE;
@@ -32,6 +37,11 @@ public class BaseResources extends Item {
     public static DeferredHolder<Item, BaseResources> QUANTUM_SCRAP;
 
     public static DeferredHolder<Item, BaseResources> QUANTUM_PROCESSOR_PRESS;
+
+    public static DeferredHolder<Item, BaseResources> LUCK_CARD_1;
+    public static DeferredHolder<Item, BaseResources> LUCK_CARD_2;
+    public static DeferredHolder<Item, BaseResources> LUCK_CARD_3;
+    public static DeferredHolder<Item, BaseResources> LUCK_CARD_4;
 
     public BaseResources(Properties properties) {
         super(properties);
