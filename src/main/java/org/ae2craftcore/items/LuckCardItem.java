efa4860d690dbe956e463fae78df387173b5f56b
dertex909 <codshannw@gmail.com>
2026-06-17 01:08:30 +0500

    сука да

--- src/main/java/org/ae2craftcore/items/LuckCardItem.java
+++ src/main/java/org/ae2craftcore/items/LuckCardItem.java
@@ -1,13 +1,8 @@
 package org.ae2craftcore.items;
 
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.network.chat.Component;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import org.ae2craftcore.registry.annotations.RegisterItem;
-import org.jetbrains.annotations.NotNull;
-import java.util.List;
 import appeng.items.materials.UpgradeCardItem;
 
 @RegisterItem(name = "luck_card_1", stacksTo = 16)
@@ -23,10 +18,5 @@ public class LuckCardItem extends UpgradeCardItem {
 
     public LuckCardItem(Properties properties) {
         super(properties);
-    }
-
-    @Override
-    public void appendHoverText(@NotNull ItemStack stack, @NotNull TooltipContext context, @NotNull List<Component> tooltipComponents, @NotNull TooltipFlag tooltipFlag) {
-        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
     }
 }
