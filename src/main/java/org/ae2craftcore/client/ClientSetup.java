ccbeaaecdc549f94bcf6e4ebea7a156b1c67174b
dertex909 <codshannw@gmail.com>
2026-07-07 21:10:17 +0500

    да

--- src/main/java/org/ae2craftcore/client/ClientSetup.java
+++ src/main/java/org/ae2craftcore/client/ClientSetup.java
@@ -1,25 +1,50 @@
 package org.ae2craftcore.client;
 
-import appeng.api.util.AEColor;
+import appeng.api.client.StorageCellModels;
 import appeng.client.render.StaticItemColor;
 import net.minecraft.client.RecipeBookCategories;
-import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.resources.ResourceLocation;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.event.ModelEvent;
 import net.neoforged.neoforge.client.event.RegisterColorHandlersEvent;
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
 import net.neoforged.neoforge.client.event.RegisterRecipeBookCategoriesEvent;
-import org.ae2craftcore.Ae2craftcore;
 import org.ae2craftcore.client.screen.*;
+import org.ae2craftcore.items.RecipeStorageCellItem;
 import org.ae2craftcore.registry.ModMenuTypes;
-import org.ae2craftcore.registry.ModRecipeTypes;
 
+import static appeng.api.util.AEColor.TRANSPARENT;
+import static net.minecraft.core.registries.BuiltInRegistries.ITEM;
 import static net.minecraft.world.item.Items.AIR;
+import static net.neoforged.api.distmarker.Dist.CLIENT;
+import static org.ae2craftcore.Ae2craftcore.MODID;
+import static org.ae2craftcore.registry.ModRecipeTypes.LOGIC_ASSEMBLING_TYPE;
 
-@EventBusSubscriber(modid = Ae2craftcore.MODID, value = Dist.CLIENT)
+@EventBusSubscriber(modid = MODID, value = CLIENT)
 public class ClientSetup {
+
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        event.enqueueWork(() -> {
+            StorageCellModels.registerModel(RecipeStorageCellItem.RECIPE_STORAGE_CELL_1K.get(), ResourceLocation.fromNamespaceAndPath(MODID, "block/drive/cells/recipe_storage_cell_1k"));
+            StorageCellModels.registerModel(RecipeStorageCellItem.RECIPE_STORAGE_CELL_4K.get(), ResourceLocation.fromNamespaceAndPath(MODID, "block/drive/cells/recipe_storage_cell_4k"));
+            StorageCellModels.registerModel(RecipeStorageCellItem.RECIPE_STORAGE_CELL_16K.get(), ResourceLocation.fromNamespaceAndPath(MODID, "block/drive/cells/recipe_storage_cell_16k"));
+            StorageCellModels.registerModel(RecipeStorageCellItem.RECIPE_STORAGE_CELL_64K.get(), ResourceLocation.fromNamespaceAndPath(MODID, "block/drive/cells/recipe_storage_cell_64k"));
+            StorageCellModels.registerModel(RecipeStorageCellItem.RECIPE_STORAGE_CELL_256K.get(), ResourceLocation.fromNamespaceAndPath(MODID, "block/drive/cells/recipe_storage_cell_256k"));
+        });
+    }
+
+    @SubscribeEvent
+    public static void registerAdditionalModels(ModelEvent.RegisterAdditional event) {
+        event.register(ModelResourceLocation.standalone(ResourceLocation.fromNamespaceAndPath(MODID, "block/drive/cells/recipe_storage_cell_1k")));
+        event.register(ModelResourceLocation.standalone(ResourceLocation.fromNamespaceAndPath(MODID, "block/drive/cells/recipe_storage_cell_4k")));
+        event.register(ModelResourceLocation.standalone(ResourceLocation.fromNamespaceAndPath(MODID, "block/drive/cells/recipe_storage_cell_16k")));
+        event.register(ModelResourceLocation.standalone(ResourceLocation.fromNamespaceAndPath(MODID, "block/drive/cells/recipe_storage_cell_64k")));
+        event.register(ModelResourceLocation.standalone(ResourceLocation.fromNamespaceAndPath(MODID, "block/drive/cells/recipe_storage_cell_256k")));
+    }
 
     @SubscribeEvent
     public static void registerScreens(RegisterMenuScreensEvent event) {
@@ -30,14 +55,14 @@ public class ClientSetup {
 
     @SubscribeEvent
     public static void registerRecipeBookCategories(RegisterRecipeBookCategoriesEvent event) {
-        event.registerRecipeCategoryFinder(ModRecipeTypes.LOGIC_ASSEMBLING_TYPE.get(), recipe -> RecipeBookCategories.CRAFTING_MISC);
+        event.registerRecipeCategoryFinder(LOGIC_ASSEMBLING_TYPE.get(), recipe -> RecipeBookCategories.CRAFTING_MISC);
     }
 
     @SubscribeEvent
     public static void registerItemColors(RegisterColorHandlersEvent.Item event) {
-        var recipeTerminalItem = BuiltInRegistries.ITEM.get(ResourceLocation.fromNamespaceAndPath(Ae2craftcore.MODID, "recipe_terminal"));
+        var recipeTerminalItem = ITEM.get(ResourceLocation.fromNamespaceAndPath(MODID, "recipe_terminal"));
         if (recipeTerminalItem != AIR) {
-            var baseColor = new StaticItemColor(AEColor.TRANSPARENT);
+            var baseColor = new StaticItemColor(TRANSPARENT);
             event.register((stack, tintIndex) -> {
                 int color = baseColor.getColor(stack, tintIndex);
                 return color == -1 ? -1 : (color | 0xFF000000);
