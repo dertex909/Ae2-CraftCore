3342cdc223a0596f977f44fc81f8a9736e21e8a1
dertex909 <codshannw@gmail.com>
2026-07-12 00:11:16 +0500

    Исправляю опечатку в тексте лицензии в LogicAssemblerMenu.

--- src/main/java/org/ae2craftcore/blocks/menu/LogicAssemblerMenu.java
+++ src/main/java/org/ae2craftcore/blocks/menu/LogicAssemblerMenu.java
@@ -3,7 +3,7 @@
  * Copyright (C) 2026 dertex909
  *
  * This program is free software; you can redistribute it and/or modify
- * taxation of the GNU Lesser General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
