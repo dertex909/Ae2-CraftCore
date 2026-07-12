e64938bff178959bbc7516823c367cb43b50b09f
dertex909 <codshannw@gmail.com>
2026-07-12 15:49:47 +0500

    Добавляю тикер для обновления кэша рецептов в MeMachineInterface.

--- src/main/java/org/ae2craftcore/blocks/block/MeMachineInterfaceBlock.java
+++ src/main/java/org/ae2craftcore/blocks/block/MeMachineInterfaceBlock.java
@@ -29,6 +29,8 @@ import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.neoforged.neoforge.registries.DeferredHolder;
@@ -110,5 +112,11 @@ public class MeMachineInterfaceBlock extends BaseEntityBlock {
             }
             super.onRemove(state, level, pos, newState, isMoving);
         }
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(@NotNull Level level, @NotNull BlockState state, @NotNull BlockEntityType<T> type) {
+        return createTickerHelper(type, MeMachineInterfaceBlockEntity.TYPE, MeMachineInterfaceBlockEntity::tick);
     }
 }
--- src/main/java/org/ae2craftcore/blocks/blockentity/MeMachineInterfaceBlockEntity.java
+++ src/main/java/org/ae2craftcore/blocks/blockentity/MeMachineInterfaceBlockEntity.java
@@ -56,6 +56,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.neoforge.capabilities.Capabilities;
@@ -63,6 +64,7 @@ import org.ae2craftcore.blocks.block.MeMachineInterfaceBlock;
 import org.ae2craftcore.blocks.menu.MeMachineInterfaceMenu;
 import org.ae2craftcore.registry.annotations.RegisterBlockEntity;
 import org.ae2craftcore.services.IRecipeCacheService;
+import org.ae2craftcore.services.RecipeCacheService;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -86,6 +88,9 @@ public class MeMachineInterfaceBlockEntity extends AENetworkedPoweredBlockEntity
     private int priority = 0;
     private final ConfigManager configManager = new ConfigManager(this::setChanged);
 
+    private int updateCooldown = 0;
+    private long lastSignature = -1;
+
     public MeMachineInterfaceBlockEntity(BlockPos pos, BlockState state) {
         super(TYPE, pos, state);
         this.configManager.registerSetting(Settings.BLOCKING_MODE, YesNo.NO);
@@ -94,6 +99,27 @@ public class MeMachineInterfaceBlockEntity extends AENetworkedPoweredBlockEntity
         this.getMainNode().addService(ICraftingProvider.class, this).setFlags(GridFlags.REQUIRE_CHANNEL).setIdlePowerUsage(1000);
         this.setInternalMaxPower(10000);
         this.setPowerSides(getGridConnectableSides(getOrientation()));
+    }
+
+    public static void tick(Level level, BlockPos pos, BlockState state, MeMachineInterfaceBlockEntity blockEntity) {
+        if (level.isClientSide) return;
+
+        if (blockEntity.updateCooldown-- <= 0) {
+            blockEntity.updateCooldown = 20;
+
+            var grid = blockEntity.getMainNode().getGrid();
+            if (grid != null) {
+                var cacheService = grid.getService(IRecipeCacheService.class);
+                if (cacheService instanceof RecipeCacheService recipeCache) {
+                    long currentSignature = recipeCache.calculateGridSignature();
+                    if (currentSignature != blockEntity.lastSignature) {
+                        blockEntity.lastSignature = currentSignature;
+                        recipeCache.invalidate();
+                        ICraftingProvider.requestUpdate(blockEntity.getMainNode());
+                    }
+                }
+            }
+        }
     }
 
     public void updateAdjacentMachine() {
