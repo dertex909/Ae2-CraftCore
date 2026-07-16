3784bce4b0e80882b189137487f6df518a06a0cb
dertex909 <codshannw@gmail.com>
2026-07-16 18:35:49 +0500

    Удаляю неиспользуемые импорты в классе MeMachineInterfaceBlock.

--- src/main/java/org/ae2craftcore/blocks/block/MeMachineInterfaceBlock.java
+++ src/main/java/org/ae2craftcore/blocks/block/MeMachineInterfaceBlock.java
@@ -29,8 +29,6 @@ import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityTicker;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.neoforged.neoforge.registries.DeferredHolder;
